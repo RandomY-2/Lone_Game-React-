0d2b41326a3c40be7a8aa806cf113cdc7e978477
Jiahe Yan <yjh020711@ucla.edu>
2021-02-03 19:52:06 +0800

    updated view

--- react-web-game/src/components/home/home_welcome/Home_welcome.js
+++ react-web-game/src/components/home/home_welcome/Home_welcome.js
@@ -6,7 +6,7 @@ const Welcome = () => {
     return (
         <div id="welcomeSection" className="welcomeContainer welcome-container">
             <div className="welcome-Info">
-                <h1 className="welcome-Info-Heading">Welcome to Web AI Game</h1>
+                <h1 className="welcome-Info-Heading">Welcome to Lone Game</h1>
                 <p className="welcome-Info-Subheading">where you can play interactive games empowered with an AI</p>
                 <a href="#game-start-id" type="button" className="welcome-Info-Button">Show Me The Games</a>
             </div>
